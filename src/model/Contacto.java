f7b591ce5502330c18fb796333ff5099f030059f
edwsilme <eesilva2@gmail.com>
2025-05-19 12:19:20 -0500

    Proyecto de nuevo contacto

---
+++ src/model/Contacto.java
@@ -0,0 +1,39 @@
+package model;
+
+public class Contacto {
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Contacto(String nombre, String telefono, String email) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Tel: " + telefono + ", Email: " + email;
+    }
+}
